eec03bd6edc9b493ab5ec3f2ab6f83aaa905fc62
qouillyan <stojakovicdragan98@gmail.com>
2023-01-31 08:48:41 +0100

    guest api middleware ispravka

--- app/Http/Controllers/GalleriesController.php
+++ app/Http/Controllers/GalleriesController.php
@@ -10,7 +10,7 @@ class GalleriesController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api');
+        $this->middleware('guest:api');
     }
 
     public function index()
